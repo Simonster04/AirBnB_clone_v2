73b065b49de59fa932b1be1faa62db206d961d9e
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-31 16:49:31 -0500

    add a new task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,19 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls is not None:
+            bbsito = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__:
+                    bbsito[key] = value
+            return bbsito
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +62,11 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects
+        """
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
