9b9928709b86b2b32b7c2ec99feb368a0e51089f
Simonster04 <simon.pahe@gmail.com>
2020-04-11 16:23:57 -0500

    fixing chown

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,8 @@
 sudo apt-get update -y
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo "HOLA BEBE" | sudo tee /data/web_static/releases/test/index.html
-sudo rm /data/web_static/current
-sudo ln -s /data/web_static/current /data/web_static/releases/test/
-sudo chown -R ubuntu:ubuntu
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
