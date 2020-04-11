25eae987ada664623d13cbdbca14e37d3890d769
Simonster04 <simon.pahe@gmail.com>
2020-04-11 16:26:10 -0500

    fixing ln

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/current /data/web_static/releases/test/
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
