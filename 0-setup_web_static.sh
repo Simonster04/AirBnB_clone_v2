918f2f9d165489a2f4a1f39adb27beef324c2605
Simonster04 <simon.pahe@gmail.com>
2020-04-11 16:21:50 -0500

    fixing folders

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,10 @@
 #Sets up your web servers for the deployment of web_static
 sudo apt-get update -y
 sudo apt-get install -y nginx
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "HOLA BEBE" | sudo tee /data/web_static/releases/test/index.html
-rm /data/web_static/current
-ln -s /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu
-sudo sed -i '38i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo rm /data/web_static/current
+sudo ln -s /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
