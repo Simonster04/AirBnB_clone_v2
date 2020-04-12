1f7c27d7d512661f67f2fecbef0cbb7a7e54e069
Simonster04 <simon.pahe@gmail.com>
2020-04-12 18:26:22 -0500

    advanced 1

---
+++ 100-clean_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+ Contains do_clean() function
+"""
+from fabric.api import *
+from fabric.operations import *
+from os.path import isfile, isdir
+import time
+
+env.hosts = ["104.196.213.138", "3.80.40.75"]
+
+
+def do_clean(number=0):
+    """ Deletes out-of-date archives """
+    if number <= 0:
+        number = 1
+    with lcd('./versions/'):
+        len = int(sudo("ls | wc -l"))
+        local("ls -tr1 | head -n +{} | xargs -d '\n' rm -rf".
+              format(len - int(number)))
+    with cd('/data/web_static/releases/'):
+        len = int(sudo("ls | wc -l"))
+        run("ls -tr1 | head -n +{} | xargs -d '\n' rm -rf".
+            format(len - int(number)))
