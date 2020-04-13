657737e454c25d17fb6769cede00298bedb01e11
Simonster04 <simon.pahe@gmail.com>
2020-04-13 17:06:21 -0500

    advanced 2

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,6 @@
+#Puppet: sets up your web servers for the deployment of web_static
+#  Use alias inside your Nginx configuration
+exec {'http_header':
+  command  => "sudo apt-get update -y && sudo apt-get install -y nginx && sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/ && echo 'This is a test' | sudo tee /data/web_static/releases/test/index.html && sudo ln -fs /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default && sudo service nginx start",
+  provider => 'shell',
+}
